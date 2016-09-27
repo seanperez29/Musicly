749e77c9d55ebc48c6e40441dd08efbddc313967
Sean Perez <sean.perez29@gmail.com>
2016-09-27 11:52:45 -0700

    Adjust size of CollectionViewCells

--- Musicly/CategoriesCell.swift
+++ Musicly/CategoriesCell.swift
@@ -33,13 +33,3 @@ extension CategoriesCell: UICollectionViewDataSource, UICollectionViewDelegate {
         
     }
 }
-
-extension CategoriesCell: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let itemsPerRow: CGFloat = 4
-        let hardCodedPadding: CGFloat = 5
-        let itemWidth = (collectionView.bounds.width / itemsPerRow) - hardCodedPadding
-        let itemHeight = collectionView.bounds.height - (2 * hardCodedPadding)
-        return CGSize(width: itemWidth, height: itemHeight)
-    }
-}

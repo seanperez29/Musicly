8892b97d7ea2c58dcf0f8c7858a3318cb510d095
Sean Perez <sean.perez29@gmail.com>
2017-03-11 21:15:51 -0700

    Add data encapsulation to AudioTrack model

--- Musicly/AudioTrack.swift
+++ Musicly/AudioTrack.swift
@@ -11,23 +11,66 @@ import UIKit
 
 class AudioTrack {
     
-    var artistName: String!
-    var songName: String!
-    var mediaURL: String!
-    var albumURL: String!
-    var id: String!
-    var hasFavorited = false
+    private var _artistName: String!
+    private var _songName: String!
+    private var _mediaURL: String!
+    private var _albumURL: String!
+    private var _id: String!
+    private var _hasFavorited = false
+    
+    var artistName: String {
+        get {
+            return _artistName
+        } set {
+            _artistName = newValue
+        }
+    }
+    var songName: String {
+        get {
+            return _songName
+        } set {
+            _songName = newValue
+        }
+    }
+    var mediaURL: String {
+        get {
+            return _mediaURL
+        } set {
+            _mediaURL = newValue
+        }
+    }
+    var albumURL: String {
+        get {
+            return _albumURL
+        } set {
+            _albumURL = newValue
+        }
+    }
+    var id: String {
+        get {
+            return _id
+        } set {
+            _id = newValue
+        }
+    }
+    var hasFavorited: Bool {
+        get {
+            return _hasFavorited
+        } set {
+            _hasFavorited = newValue
+        }
+    }
     
     init(dictionary: [String:AnyObject]) {
-        artistName = dictionary[Constants.AudioTrack.ArtistName] as! String
-        songName = dictionary[Constants.AudioTrack.SongName] as! String
-        mediaURL = dictionary[Constants.AudioTrack.MediaURL] as! String
-        albumURL = dictionary[Constants.AudioTrack.AlbumURL] as! String
-        id = dictionary[Constants.AudioTrack.ID] as! String
+        _artistName = dictionary[Constants.AudioTrack.ArtistName] as! String
+        _songName = dictionary[Constants.AudioTrack.SongName] as! String
+        _mediaURL = dictionary[Constants.AudioTrack.MediaURL] as! String
+        _albumURL = dictionary[Constants.AudioTrack.AlbumURL] as! String
+        _id = dictionary[Constants.AudioTrack.ID] as! String
     }
     
     func toggleFavorited() {
-        hasFavorited = !hasFavorited
+        _hasFavorited = !_hasFavorited
     }
     
 }
